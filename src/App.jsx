d35c68cf0517754c60ecabecc273dd4da1e1b2d3
imVetrivel <vetrivel2002.k@gmail.com>
2024-09-20 06:23:54 +0530

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Navbar } from "./components/Navbar";
-
+import { Profile } from "./pages/Profilepage";
 const App=()=>{
 	return(
 		<>
 		<Navbar/>
-		<h1>hello</h1>
+		<Profile/>
 		</>
 	)
 }
---
+++ src/pages/Profilepage.jsx
@@ -0,0 +1,19 @@
+import '../assets/css/core.css'
+
+
+const Profile = ()=>{
+	return(
+		<>
+		<div className='flex flex-row justify-center items-center w-[60%] mx-auto'>
+			<div>
+				<h1 className='text-xl'>Hello! I am Vetrivel.K Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum provident eaque id dolores laborum reprehenderit debitis quasi? Sapiente neque illo provident voluptate voluptatibus quae atque sunt ipsam maxime, nesciunt officiis dolor pariatur earum ea eius adipisci vel culpa natus quia distinctio corporis tempora, delectus esse sint? Sequi libero praesentium cum.</h1>
+			</div>
+			<div>
+				<img src="" alt="Profile photo" />
+			</div>
+		</div>
+		</>
+	)
+}
+
+export { Profile };
